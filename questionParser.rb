a720604684ea6c916abb28430d964ae33f7c211d
Uladzislau Maltsau <Uladzislau.Maltsau@itechart-group.com>
2019-12-12 18:59:05 +0300

    Lil cleanup

--- questionParser.rb
+++
@@ -1,9 +0,0 @@
-require_relative './db'
-
-def parseQuestions()
-    question = Question.create(title: 'How do you do?')
-
-    question.answers.create(text: 'Fine')
-    question.answers.create(text: 'Awful')
-    question.answers.create(text: 'Not bad not terrible')
-end
